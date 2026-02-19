6a41b3a87a64b7d5c60c4f1b438847967cb2c0ca
reynoldsjulia789-byte <jreynolds21@ewu.edu>
2026-02-19 14:29:11 -0800

    update

--- src/ExpressionEvaluator.java
+++ src/ExpressionEvaluator.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.util.Scanner;
+
 public class ExpressionEvaluator
 {
     public static void main(String[] args)
@@ -14,6 +16,24 @@ public class ExpressionEvaluator
      */
     public int evaluatePostfixExpression(String postfixExpression)
     {
+        Scanner postfixScanner;
+
+        if (postfixExpression == null || postfixExpression.isBlank())
+        {
+            throw new IllegalArgumentException("cannot evaluate null or blank expression");
+        }
+
+        postfixScanner = new Scanner(postfixExpression);
+
+        // While there are still items in the postfix expression
+        while(postfixScanner.hasNext())
+        {
+            // if the current item is an operator, push to stack
+
+            // else pop stack & attach operand to right of operator, pop again and attach operand to left of operator,
+            // evaluate and push the results
+        }
+
         return -1;
     }
 
