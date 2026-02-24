9e5bbd6c46cfdb83b84ff7a26da98bb4828edff1
reynoldsjulia789-byte <jreynolds21@ewu.edu>
2026-02-24 11:05:02 -0800

    started tokenizer tokenize method

--- src/Model/Tokenizer.java
+++ src/Model/Tokenizer.java
@@ -1,6 +1,35 @@
 package src.Model;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Tokenizer
 {
+    /**
+     * Method that takes a string and turns it into a String ArrayList
+     * @param expression the mathematical expression to tokenize
+     * @return ArrayList of tokens, separated into operators and operands
+     */
+    public static ArrayList<String> tokenize(String expression)
+    {
+        ArrayList<String> tokens;
+        Scanner expressionScanner;
 
+        tokens            = new ArrayList<>();
+        expressionScanner = new Scanner(expression);
+
+        while (expressionScanner.hasNext())
+        {
+            if (expressionScanner.hasNextDouble())
+            {
+                tokens.addLast(expressionScanner.nextDouble() + "");
+            }
+            else
+            {
+                tokens.addLast(expressionScanner.next());
+            }
+        }
+
+        return tokens;
+    }
 }
