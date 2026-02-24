869708773f676f546fcb2335cbce6b41f511eedd
reynoldsjulia789-byte <jreynolds21@ewu.edu>
2026-02-24 10:32:00 -0800

    started writing input processor methods

--- src/Controller/InputProcessor.java
+++ src/Controller/InputProcessor.java
@@ -1,5 +1,7 @@
 package src.Controller;
 
+import src.Model.ExpressionOperations;
+import src.Model.Stack;
 import src.Model.Tokenizer;
 import src.Model.Operators;
 import static src.Model.ExpressionOperations.evaluateInfixExpressionWithDoubles;
@@ -10,7 +12,30 @@ import static src.Model.ExpressionOperations.evaluateInfixExpressionWithDoubles;
  */
 public class InputProcessor
 {
-    private StringBuilder input;
+    private StringBuilder  input;
+    private Stack<Integer> charsAdded;
 
+    public InputProcessor()
+    {
+        this.input = new StringBuilder();
+    }
+
+    public String pressed(String buttonValue)
+    {
+        this.input.append(buttonValue);
+        return this.input.toString();
+    }
+
+    public String compute()
+    {
+        Double result;
+
+        // tokenize input
+
+        // evaluate expression and return result
+        result = ExpressionOperations.evaluateInfixExpressionWithDoubles("1");
+
+        return result.toString();
+    }
 
 }
