3372752639e651438f2f2b3c87a57b7db09a888b
reynoldsjulia789 <jreynolds21@ewu.edu>
2026-03-22 20:43:28 -0700

    updated GUI to work with new project structure

--- src/Controller/InputProcessor.java
+++ src/Controller/InputProcessor.java
@@ -1,43 +1,94 @@
 package src.Controller;
 
 import src.Model.Expression;
-import src.Model.Stack;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
- * The purpose of this class is to process user input from the user interface
+ * The purpose of this class is to process user input from the GUI
  * @author Julia Reynolds
  */
 public class InputProcessor
 {
-    private StringBuilder  input;
-    private Stack<Integer> charsAdded;
+    private ArrayList<String>      input;
+    private LinkedList<Expression> history;
+    private Double                 lastResult;
 
+    /**
+     * Constructor for Input Processor
+     */
     public InputProcessor()
     {
-        this.input = new StringBuilder();
+        this.input      = new ArrayList<>();
+        this.history    = new LinkedList<>();
+        this.lastResult = null;
     }
 
+    /**
+     * Adds the value of the pressed button to the expression
+     * @param buttonValue String representing the value of the pressed button
+     * @return returns the updated expression
+     */
     public String pressed(String buttonValue)
     {
-        this.input.append(buttonValue);
+        this.input.addLast(buttonValue);
+
         return this.input.toString();
     }
 
+    /**
+     * Deletes the last input from the inputted expression.
+     * @return returns the updated expression
+     */
+    public String deleteLastInput()
+    {
+        this.input.removeLast();
+
+        return this.input.toString();
+    }
+
+    /**
+     * Computes the result of the inputted expression, and stores the result and expression.
+     * @return returns a string with the double result
+     */
     public String compute()
     {
+        Expression expression;
+
         try
         {
-            return "" + ExpressionEvaluation.evaluateInfixExpression(this.input.toString());
+            expression      = new Expression(this.input.toString());
+            this.lastResult = expression.getResult();
+
+            this.history.addFirst(expression);
+
+            this.input.clear();
+            this.input.addLast(lastResult.toString());
+
+            return this.lastResult.toString();
         }
         catch (Exception caught)
         {
-            return caught.getMessage();
+            return "Error: " + caught.getMessage();
         }
     }
 
-    public String convertExpression()
+    /**
+     * Gets the last expression stored in the history and returns it.
+     * @return returns the last infix expression
+     */
+    public String getLastExpression()
     {
-        return Expression.convertToPostfix(this.input.toString());
+        return this.history.getFirst().getInfix();
     }
 
+    /**
+     * Input processor toString
+     * @return returns the input expression as a string
+     */
+    public String toString()
+    {
+        return this.input.toString();
+    }
 }
